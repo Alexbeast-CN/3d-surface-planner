696b38e7e5b486cc31f6fdf37194dbac5345fc6a
chendaoming <chendaoming@drivedream.com>
2024-03-07 13:28:16 +0800

    (#3) render the mesh for better visualization

--- pycode/view_obj.py
+++ pycode/view_obj.py
@@ -7,6 +7,7 @@ import os
 import numpy as np
 import open3d as o3d
 
+
 def load_obj(filename: str) -> Tuple[np.ndarray, np.ndarray]:
     """
     Load vertices and faces from an OBJ file.
@@ -33,12 +34,33 @@ def load_obj(filename: str) -> Tuple[np.ndarray, np.ndarray]:
                 vertices.append(p)
             # Read facets
             elif prefix == 'f':
-                f = np.array([int(x)-1 for x in tokens[1:]], dtype=int)
+                f = np.array([int(x) - 1 for x in tokens[1:]], dtype=int)
                 faces.append(f)
 
     vertices = np.array(vertices)
     faces = np.array(faces)
     return vertices, faces
+
+
+def quad_to_tri(faces: np.ndarray) -> np.ndarray:
+    """
+    Convert quad faces to triangle faces.
+
+    Args:
+        faces (np.ndarray): The faces as a NumPy array.
+
+    Returns:
+        np.ndarray: The triangle faces as a NumPy array.
+    """
+    tri_faces = []
+    for face in faces:
+        if len(face) == 4:
+            tri_faces.append([face[0], face[1], face[2]])
+            tri_faces.append([face[0], face[2], face[3]])
+        else:
+            tri_faces.append(face)
+    return np.array(tri_faces)
+
 
 def view_obj(filepath):
     """
@@ -48,6 +70,12 @@ def view_obj(filepath):
         filepath (str): The path to the OBJ file.
     """
     vertices, faces = load_obj(filepath)
+    tri_faces = quad_to_tri(faces)
+
+    # Create mesh
+    mesh = o3d.geometry.TriangleMesh(o3d.utility.Vector3dVector(vertices),
+                                     o3d.utility.Vector3iVector(tri_faces))
+    mesh.paint_uniform_color([0.5, 0.5, 0.5])  # grey
 
     # Create a point cloud
     points = o3d.geometry.PointCloud()
@@ -56,7 +84,7 @@ def view_obj(filepath):
     lines_list = []
     for face in faces:
         for i, _ in enumerate(face):
-            lines_list.append([face[i], face[(i+1)%len(face)]])
+            lines_list.append([face[i], face[(i + 1) % len(face)]])
 
     lines = np.array(lines_list, dtype=int)
 
@@ -65,13 +93,23 @@ def view_obj(filepath):
     line_set.points = o3d.utility.Vector3dVector(vertices)
     line_set.lines = o3d.utility.Vector2iVector(lines)
 
-    # Set point color to gray
-    colors = np.array([[0.5, 0.5, 0.5] for _ in range(len(vertices))])
+    # Set point color to black
+    colors = np.array([[0, 0, 0] for _ in range(len(vertices))])
     points.colors = o3d.utility.Vector3dVector(colors)
 
     # Visualize
     # pylint: disable=E1101
-    o3d.visualization.draw_geometries([line_set, points])
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
+
+    # Add objs to the visualizer
+    vis.add_geometry(mesh)
+    vis.add_geometry(line_set)
+    vis.add_geometry(points)
+
+    # Run the visualizer
+    vis.run()
+
 
 def main():
     """
@@ -79,11 +117,12 @@ def main():
     """
     # Load the .obj file
     cur_file_path = os.path.dirname(os.path.realpath(__file__))
-    file_name = 'spoon_quad.obj'
+    file_name = 'dragon_quad.obj'
     obj_file_path = os.path.join(cur_file_path, '../data', file_name)
 
     # Visualize the object
     view_obj(obj_file_path)
+
 
 if __name__ == '__main__':
     main()
